fb55c72801e938a9bdbdee353d8c107f2c9b182d
Nikhil <nikhil.soin@groww.in>
2023-09-04 00:56:57 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const main = async () => {
+  try {
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     **/
+    const owner = github.repository_owner;
+    const repo = github.repository;
+    const pr_number = github.event.pull_request.number;
+    const token = core.getInput("token", { required: true });
+    const upperLimit = core.getInput("upperLimit");
+
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * GitHub's REST API endpoints.
+     * We will pass the token as an argument to the constructor. This token
+     * will be used to authenticate our requests.
+     * You can find all the information about how to use Octokit here:
+     * https://octokit.github.io/rest.js/v18
+     **/
+    const octokit = new github.getOctokit(token);
+
+    /**
+     * We need to fetch the list of files that were changes in the Pull Request
+     * and store them in a variable.
+     * We use octokit.paginate() to automatically loop over all the pages of the
+     * results.
+     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+     */
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: pr_number,
+    });
+
+    /**
+     * Contains the sum of all the additions, deletions, and changes
+     * in all the files in the Pull Request.
+     **/
+    let diffData = {
+      additions: 0,
+      deletions: 0,
+      changes: 0,
+    };
+
+    // Reference for how to use Array.reduce():
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions;
+      acc.deletions += file.deletions;
+      acc.changes += file.changes;
+      return acc;
+    }, diffData);
+
+    /**
+     * Create a comment on the PR with the information we compiled from the
+     * list of changed files.
+     */
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+        Pull Request #${pr_number} has been updated with: \n
+        - ${diffData.changes} changes \n
+        - ${diffData.additions} additions \n
+        - ${diffData.deletions} deletions \n
+        = As the number of change in PR are greater then upper limit set by release mandate please do break down your pr into small PR\n
+          Upper Limit: ${upperLimit} 
+          Pr file count: ${changedFiles.length}
+
+      `,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+// Call the main function to run the action
+main();
